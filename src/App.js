08ed8121a8e6ba10bb678e9475df73494ae88563
Nazarenko Yurii <wallpapeforpc@gmail.com>
2023-03-31 21:58:44 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
 
 function App() {
+  let clicksValue = localStorage.getItem('value') || 0;
+  const [count, setCount] = useState(clicksValue);
+
+  function increase() {
+    clicksValue++;
+    localStorage.setItem('value', clicksValue);
+    setCount(clicksValue);
+  }
+
+  function decrease() {
+    clicksValue--;
+    localStorage.setItem('value', clicksValue);
+    setCount(clicksValue);
+  }
+
+  function reset() {
+    localStorage.clear();
+    clicksValue = 0;
+    setCount(clicksValue);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container my-5'>
+        <h1 className='my-3'>Counter App</h1>
+        <div className='card-body'>
+          <h2 className='my-5'>{count}</h2>
+          <button className='btn btn-danger mx-1' onClick={decrease}>Decrease</button>
+          <button className='btn btn-warning text-light mx-1' onClick={reset}>Reset</button>
+          <button className='btn btn-success mx-1' onClick={increase}>Increase</button>
+        </div>
+      </div>
     </div>
   );
 }
